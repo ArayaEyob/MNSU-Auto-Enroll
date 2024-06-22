0fed33760505d9072247667d0c69c97d37c60539
ArayaEyob <arayacap@icloud.com>
2024-06-22 12:03:38 -0500

    method to check chart is empty

--- send-sms.py
+++ send-sms.py
@@ -24,3 +24,39 @@ user_phone_number = "+1" + "4132424123"
 star_id = "Jo3165wl"
 password = "Mancitylove2@"
 
+
+client = Client(account_sid, auth_token)
+
+options = Options()
+options.add_argument("--start-maximized")
+options.add_argument("--remote-allow-origins")
+driver = webdriver.chrome(options=options)
+
+def send_sms(to, message):
+    client.messages.create(
+        body=message,
+        from_=twilio_phone_number,
+        to=to
+    )
+
+# Method to navigate to the shopping cart
+def go_to_shopping_cart(driver):
+    driver.get("https://eservices.minnstate.edu/esession/authentication.do?campusId=071&postAuthUrl=http%3A%2F%2Feservices.minnstate.edu%2Fstudent-portal%2Fsecure%2Fdashboard.do%3Fcampusid%3D071")
+    if cart_is_empty(driver):
+        try:
+            driver.find_element(By.CSS_SELECTOR, "span[id='DERIVED_SSR_FL_SSR_NOCLASSES_MSG$88$']")
+        except:
+            for i in range(13):
+                try:
+                    driver.find_element(By.CSS_SELECTOR, f"a[id='SSR_CART_TRM_FL_TERM_DESCR30${i}']").click()
+                    break
+                except:
+                    continue
+
+def cart_is_empty(driver):
+    try:
+        driver.find_element(By.CSS_SELECTOR, "span[id='DERIVED_SSR_FL_SSR_AVAIL_FL$0']")
+    except:
+        print("Shopping Cart is Empty")
+        return True
+    return False
