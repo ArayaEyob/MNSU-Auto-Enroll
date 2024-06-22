776963ca04c5497b1aff4c2cf7309d6fe4f39596
ArayaEyob <arayacap@icloud.com>
2024-06-22 11:59:01 -0500

    imported the neccassry items

---
+++ send-sms.py
@@ -0,0 +1,14 @@
+import os 
+import time
+import re
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import exxpected_conditions as EC
+from twilio.rest import Client
+
+
+
+
